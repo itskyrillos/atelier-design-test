b659334de982a363d02224373e39187aa287d5a7
Cyrille Guillaume <cyrilleguillaume1996@gmail.com>
2021-12-13 16:59:52 +0100

    Loader is working

--- src/js/loader.js
+++ src/js/loader.js
@@ -11,16 +11,15 @@ tl.from('.loader__image',
     skewY: 9
   }
 ).to('.loader__image', {
-  delay: 1,
+  delay: 0.5,
   duration: 1.4,
   y: -300,
   ease: 'power4.in',
   skewY: 9,
   transformOrigin: 'right center'
 }).to('.loader', {
-  delay: -0.2,
   duration: 1.5,
   ease: 'power4.out',
   transformOrigin: 'top left',
   scaleY: 0
-})
+}, '>-0.1')
